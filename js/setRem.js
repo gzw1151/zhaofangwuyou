8c2758fe03d23344162908b77ce4e68e96d033b2
黄卫视 <3261144270@qq.com>
2022-11-16 19:26:29 +0800

    '完成'

--- js/setRem.js
+++ js/setRem.js
@@ -1,13 +1,22 @@
-function setRem(){
-    var ui_w=375;
-    var client_w=document.documentElement.clientWidth || document.body.clientWidth;
-    var html=document.getElementsByTagName('html')[0];
-    html.style.fontSize = (client_w/ui_w)*10+'px';
+
+function setRem() {
+    var ui_w = 375;
+    var client_w = document.documentElement.clientWidth || document.body.clientWidth;
+
+    // 如果设备宽度大于600  等于600
+    client_w = client_w > 600 ? 600 : client_w;
+    client_w = client_w < 250 ? 250 : client_w;
+    console.log(ui_w, client_w);
+    var html = document.getElementsByTagName('html')[0];
+    html.style.fontSize = (client_w / ui_w) * 10 + 'px';
 }
-var timer=null;
-window.onresize=function() {
+
+var timer = null;
+
+
+
+
+window.onresize = function () {
     clearTimeout(timer);
-    timer=setTimeout(function() {
-        setRem();
-    },1000)
-}
+    timer = setTimeout(setRem, 300);
+};
